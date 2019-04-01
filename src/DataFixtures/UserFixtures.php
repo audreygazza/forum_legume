dc955e42bcd62ecca5b81ede04d06f528a90895a
Audrey Gazza <gazzaaudrey@gmail.com>
2019-04-01 16:56:07 +0200

    modif cryptage

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -5,9 +5,19 @@ namespace App\DataFixtures;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 use App\Entity\User;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class UserFixtures extends Fixture
 {
+
+    private $passwordEncoder;
+
+    //méthode pour hacher le mot de passe
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+      $this->passwordEncoder = $passwordEncoder;
+    }
+
     public function load(ObjectManager $manager)
     {
         // $product = new Product();
@@ -16,17 +26,17 @@ class UserFixtures extends Fixture
         $user1 = new User();
         $user1 -> setEmail("oceane.allix@outlook.com")
          -> setPseudo("Pablo")
-         -> setPassword("blingbling");
+         -> setPassword($this->passwordEncoder->encodePassword($user1, "blingbling"));
 
          $user2 = new User();
          $user2 -> setEmail("oceane.allix@outlook.com")
           -> setPseudo("Salome")
-          -> setPassword("bling");
+          -> setPassword($this->passwordEncoder->encodePassword($user2, "bling"));
 
           $user3 = new User();
           $user3 -> setEmail("oceane.allix@outlook.com")
            -> setPseudo("regl@ss")
-           -> setPassword("bling");
+           -> setPassword($this->passwordEncoder->encodePassword($user3, "bling"));
 
         $manager->flush();
     }
