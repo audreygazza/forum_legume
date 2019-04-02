771f9e71eb2ed0e90dd6b14c7fe30c9be012eca2
OceaneAllix <oceane.alli8@gmail.com>
2019-04-02 17:06:27 +0200

    crud message

---
+++ src/Form/MessageType.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Message;
+use App\Entity\User;
+use App\Entity\Discussion;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+
+
+
+
+class MessageType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title', TextType::class)
+            ->add('content', TextareaType::class)
+            ->add('created_at', DateType::class)
+            ->add('user',EntityType::class,[
+              'class'=> User::class,
+              'choice_label'=>'pseudo'
+            ])
+            ->add('discussion',EntityType::class,[
+              'class'=> discussion::class,
+              'choice_label'=>'title'
+              ] )
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Message::class,
+            'label_format' => 'message.%name%'
+
+        ]);
+    }
+}
