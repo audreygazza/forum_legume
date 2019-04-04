453fb17a4f963407299cfe0a196e4dc89f04d796
Audrey Gazza <gazzaaudrey@gmail.com>
2019-04-04 11:21:36 +0200

    insertion de slug

--- src/DataFixtures/ThemeFixtures.php
+++ src/DataFixtures/ThemeFixtures.php
@@ -19,31 +19,41 @@ class ThemeFixtures extends Fixture
 
         $theme1 = new Theme();
         $theme1 -> setName("Coup d'coeur conso")
-                -> setDescription("Partagez vos coups de coeurs cuisine, légumes ou producteurs.");
+                -> setDescription("Partagez vos coups de coeurs cuisine, légumes ou producteurs.")
+                -> setSlug('coup_d_coeur_conso');
+
 
         $manager->persist($theme1);
 
         $theme2 = new Theme();
         $theme2 -> setName("Coup d'gueule conso")
-                -> setDescription("Vous avez repéré des arnaques ou des produits de mauvaise qualités, faites-en part !!");
+                -> setDescription("Vous avez repéré des arnaques ou des produits de mauvaise qualités, faites-en part !!")
+                -> setSlug('coup_d_gueule_conso');
+
 
         $manager->persist($theme2);
 
         $theme3 = new Theme();
         $theme3 -> setName("Bons plans")
-                -> setDescription("Partagez ici les bons plans que vous avez repéré.");
+                -> setDescription("Partagez ici les bons plans que vous avez repéré.")
+                -> setSlug('bons_plans');
+
 
         $manager->persist($theme3);
 
         $theme4 = new Theme();
         $theme4 -> setName("Avis sur produits/Marques")
-                -> setDescription("Aidez-nous à consommer responsable. N'hésitez pas à partager circuits courts et trouvailles.");
+                -> setDescription("Aidez-nous à consommer responsable. N'hésitez pas à partager circuits courts et trouvailles.")
+                -> setSlug('avis_sur_produits');
+
 
         $manager->persist($theme4);
 
         $theme5 = new Theme();
         $theme5 -> setName("Conseils & Astuces")
-                -> setDescription("Partagez ici les conseils cuisine, achat ou consommation.");
+                -> setDescription("Partagez ici les conseils cuisine, achat ou consommation.")
+                -> setSlug('conseil');
+                
 
         $manager->persist($theme5);
 
