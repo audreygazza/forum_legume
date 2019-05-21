d67f7a0be08f3f3a9444ffe5191044936667dc10
taourirt taoufik <ttaourirt@live.fr>
2019-05-21 17:11:50 +0200

    Ajout d'utilisateur

--- src/DataFixtures/PersonFixtures.php
+++ src/DataFixtures/PersonFixtures.php
@@ -16,8 +16,22 @@ class PersonFixtures extends Fixture implements DependentFixtureInterface
         // $manager->persist($product);
           $person1 = new Person();
           $person1->setImage('')
-              ->setUser($this->getReference(UserFixtures::USER_SALOME));
+              ->setUser($this->getReference(UserFixtures::USER_PABLO));
           $manager->persist($person1);
+
+        $manager->flush();
+
+          $person2 = new Person();
+          $person2->setImage('')
+              ->setUser($this->getReference(UserFixtures::USER_SALOME));
+          $manager->persist($person3);
+
+        $manager->flush();
+
+          $person3 = new Person();
+          $person3->setImage('')
+              ->setUser($this->getReference(UserFixtures::USER_REGLASS));
+          $manager->persist($person3);
 
         $manager->flush();
     }
