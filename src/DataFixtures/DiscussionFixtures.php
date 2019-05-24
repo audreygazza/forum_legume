6a645394ea9522cc6e154ba05bef91d84ef34994
OceaneAllix <oceane.alli8@gmail.com>
2019-05-24 17:16:31 +0200

    fixture discussion

--- src/DataFixtures/DiscussionFixtures.php
+++ src/DataFixtures/DiscussionFixtures.php
@@ -12,6 +12,10 @@ class DiscussionFixtures extends Fixture implements DependentFixtureInterface
   const DISCUSSION_AMAP = 'disc1';
   const DISCUSSION_ALERTE = 'disc2';
   const DISCUSSION_DRIVE = 'disc3';
+  const DISCUSSION_SUPERALIMENTS = 'disc4';
+  const DISCUSSION_ALIMENTSSAINS = 'disc5';
+
+
 
     public function load(ObjectManager $manager)
     {
@@ -36,11 +40,27 @@ class DiscussionFixtures extends Fixture implements DependentFixtureInterface
 
         $manager->persist($discussion3);
 
+        $discussion4 = new Discussion();
+        $discussion4->setTitle("Les superaliments ou les aliments miracles")
+          ->setTheme($this->getReference(ThemeFixtures::THEME_COEUR));
+
+        $manager->persist($discussion4);
+
+        $discussion5 = new Discussion();
+        $discussion5->setTitle("Aliments sains")
+          ->setTheme($this->getReference(ThemeFixtures::THEME_COEUR));
+
+        $manager->persist($discussion5);
+
         $manager->flush();
 
         $this->addReference(self::DISCUSSION_AMAP, $discussion1);
         $this->addReference(self::DISCUSSION_ALERTE, $discussion2);
         $this->addReference(self::DISCUSSION_DRIVE, $discussion3);
+        $this->addReference(self::DISCUSSION_SUPERALIMENTS, $discussion4);
+        $this->addReference(self::DISCUSSION_ALIMENTSSAINS, $discussion5);
+
+
 
     }
 
